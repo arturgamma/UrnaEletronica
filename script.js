416d630a040121993403b33d54924b02b35bf8b1
João Artur Rodrigues <arturgamma@gmail.com>
2021-01-09 00:41:55 -0300

    script inicializado

--- script.js
+++ script.js
@@ -3,3 +3,40 @@ let seuVotoPara = document.getElementById('seuVotoPara')
 let descricao = document.getElementById('descricao')
 let infoTela = document.getElementById('infoTela')
 let latDireita = document.getElementById('latDireita')
+let numeros = document.getElementById('numeros')
+
+let etapaAtual = 0;
+
+function iniciarEtapa() {
+    let numeroHtml = ''
+    for (let i = 0; i < etapas.numeros; i++) {
+        numeroHtml += '<div class="numero"></div>'
+    }
+    let etapa = etapas[etapaAtual]
+    seuVotoPara.style.display = 'none'
+    descricao.innerHTML = ''
+    infoTela.innerHTML = ''
+    latDireita.innerHTML = ''
+    numeros.innerHTML = numeroHtml
+
+
+
+}
+
+function clicou(n) {
+    alert("Clicou em " + n)
+}
+
+function branco() {
+    alert("Clicou em branco!")
+}
+
+function corrige() {
+    alert("Clicou em corrige!")
+}
+
+function confirma() {
+    alert("Clicou em confirma!")
+}
+
+iniciarEtapa()
